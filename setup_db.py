5e133c78427fccf2856391a53ecca72f99cf401e
Zac Baker <zacattackbaker@gmail.com>
2024-07-24 22:43:17 -0400

    Revert "Add database setup script"
    This reverts commit e3a47b88520e162a6fb4d12ea873812096673674.

--- setup_db.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-
-def setup_database():
-    with app.app_context():
-        db.create_all()
-        print("Database tables created successfully.")
-
-if __name__ == "__main__":
-    setup_database()
